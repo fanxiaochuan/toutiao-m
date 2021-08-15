102c6a28e81ee6b2f9670d1ce132c655e4abb6fd
fanxiaochuan <xfan718@gmail.com>
2021-08-15 14:01:18 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import './styles/index.less'
 Vue.config.productionTip = false
+//  加载全局样式
 
 new Vue({
   router,
